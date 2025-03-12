a32c11d84942256386fe795082d5716248ba5b16
Luthfi <luthfihadi543@gmail.com>
2025-03-12 12:54:45 +0700

    update content

--- src/data/cvData.ts
+++ src/data/cvData.ts
@@ -43,7 +43,7 @@ export interface Skills {
   Language: string[];
   "Soft Skills": string[];
   "Hard Skills": string[];
-  Software: string[];
+  "Tool & Software": string[];
 }
 
 export interface Project {
@@ -51,6 +51,7 @@ export interface Project {
   image: string;
   description: string;
   techStack: string[];
+  link?: string;
 }
 
 export interface CVSection {
@@ -68,7 +69,7 @@ export const isSkills = (content: any): content is Skills => {
     "Language" in content &&
     "Soft Skills" in content &&
     "Hard Skills" in content &&
-    "Software" in content
+    "Tool & Software" in content
   );
 };
 
@@ -102,40 +103,27 @@ export const isProject = (content: any): content is Project => {
 export const skills: Skills = {
   Language: [
     "Indonesia (Native or bilingual proficiency)",
-    "English (Limited working proficiency)",
-    "Arabic (Elementary proficiency)",
+    "English (Professional working proficiency)",
   ],
   "Soft Skills": [
     "Communication",
     "Leadership",
-    "Team Management",
-    "Organizational Skills",
-    "Problem Solving",
-    "Critical Thinking",
+    "Collaboration",
+    "Problem-Solving",
+    "Code Review",
+    "Adaptability",
   ],
   "Hard Skills": [
-    "HTML/CSS",
-    "JavaScript",
-    "TypeScript",
-    "React",
-    "Tailwind CSS",
-    "Bootstrap",
-    "ASP.NET",
-    "C#",
-    "Laravel",
-    "PHP",
-    "MySQL",
-    "WordPress",
-    "C",
-    "Java",
-    "Python",
+    "Frontend: HTML, CSS, JavaScript, TypeScript, React, Next.js, Chakra UI, Tailwind CSS, Bootstrap",
+    "Backend: Golang (Echo, GORM), ASP.NET, C#, Laravel, PHP",
+    "Database: MySQL, PostgreSQL",
+    "Other: Motoko, C, Java, Python, WordPress",
   ],
-  Software: [
+  "Tool & Software": [
     "Google Workspace",
     "Microsoft 365",
     "Figma",
     "Canva",
-    "WordPress",
     "Adobe Premiere Pro",
     "Capcut",
     "Visual Studio Code",
@@ -150,25 +138,43 @@ export const projects = [
     title: "Luthfi Portfolio Website",
     image: "./project-1.png",
     description:
-      "My portfolio website showcasing my projects and skills in software engineering.",
+      "My portfolio website showcasing my projects & skills with perfect Lighthouse scores: 100 in Performance, Accessibility, Best Practices, and SEO",
     techStack: ["HTML/CSS", "JavaScript"],
     link: "https://bit.ly/Luthfi-Web-Portfolio",
   },
   {
-    title: "Luthfi CV Website",
+    title: "LMS Dummy Frontend",
     image: "./project-2.png",
     description:
-      "My first React project, featuring a responsive CV with type-safe development.",
-    techStack: ["React", "TypeScript", "Tailwind CSS"],
-    link: "https://luthficv.vercel.app",
+      "A prototype for an LMS, focusing on UI/UX implementation and responsiveness.",
+    techStack: [
+      "React",
+      "TypeScript",
+      "Chakra UI",
+      "React Router",
+      "Zustand",
+      "Tanstack",
+      "Recharts",
+      "FullCalendar",
+    ],
+    link: "https://university-lms-seven.vercel.app/",
   },
   {
-    title: "PSD Project GymMe",
+    title: "React Form Handling",
     image: "./project-3.png",
     description:
-      "A group project for a supplement purchase platform with customer and admin functionalities.",
-    techStack: ["ASP.NET", "C#", "Tailwind CSS"],
-    link: "https://github.com/luthfidi/PSD-Project-GymMe",
+      "A comprehensive guide to building forms with React Hook Form and Zod.",
+    techStack: [
+      "React",
+      "TypeScript",
+      "React Hook Form",
+      "Zod",
+      "Chakra UI",
+      "React Router",
+      "Zustand",
+      "Tanstack",
+    ],
+    link: "https://react-form-mastery.vercel.app/",
   },
 ];
 
@@ -179,7 +185,7 @@ export const cvSections: { [key: string]: CVSection } = {
     content: [
       {
         description:
-          "A 6th-semester Computer Science student at BINUS University with a background in digital marketing, SEO, and content creation. Experienced in project management and team leadership. Seeking to transition focus to software engineering, specifically as a front-end developer.",
+          "A 6th-semester Computer Science student at BINUS University with a strong foundation in front-end development and software engineering. Experienced in building responsive web applications, optimizing website performance, and collaborating in team-based projects. Previously involved in digital marketing and SEO, bringing a unique perspective on user experience and engagement.",
       },
     ],
   },
@@ -192,10 +198,9 @@ export const cvSections: { [key: string]: CVSection } = {
         institution: "Bina Nusantara University",
         period: "2022 – Present",
         details: [
-          "Achieved an A grade in Agile Software Development, Code Reengineering, Pattern Software Design, and Human and Computer Interaction",
-          "Gained skills in creating user-friendly software interfaces and enhancing user experience",
-          "Developed abilities in optimizing code performance and maintaining code quality",
-          "Acquired practical skills in Agile methodologies and Scrum for project management",
+          "Achieved an A grade in Agile Development, Pattern Software Design, and Human Computer Interaction.",
+          "Developed abilities in optimizing code performance and maintaining code quality.",
+          "Acquired practical skills in Agile methodologies and Scrum for project management.",
         ],
       },
       {
@@ -203,8 +208,8 @@ export const cvSections: { [key: string]: CVSection } = {
         institution: "Ibnu Hajar Boarding School",
         period: "2018 – 2021",
         details: [
-          "Obtained eligible quota for SNMPTN",
-          "Head of Entrepreneurship Division at OSMIB 2020"
+          "Obtained eligible quota for SNMPTN.",
+          "Head of Entrepreneurship Division at OSMIB 2020.",
         ],
       },
     ],
@@ -214,15 +219,27 @@ export const cvSections: { [key: string]: CVSection } = {
     icon: Briefcase,
     content: [
       {
+        title: "Full Stack Developer",
+        company: "PT Zegen Solusi Mandiri",
+        period: "Feb/2025 – Present",
+        details: [
+          "Developing a Learning Management System (LMS) using React, Golang, Chakra UI, and modern libraries.",
+          "Designing and implementing ERD to optimize database structure and improve query efficiency.",
+          "Slicing UI designs into responsive and interactive web components.",
+          "Building 10+ mini projects as prototypes to refine LMS core features and improve development efficiency.",
+          "Optimizing website performance by reducing load time and improving accessibility.",
+        ],
+      },
+      {
         title: "SEO Backlinking Specialist",
         company: "PT Laskar Digital Indonesia",
         period: "Sep/2021 – Dec/2021",
         details: [
-          "Contributed to ranking 40+ websites/articles on Google's first page for targeted keywords with SEO team",
-          "Published 174 articles with SEO standards",
-          "Curated 150 high-authority social bookmarking websites (minimum DA 30, PA 30)",
-          "Social bookmarked 15 articles across a total of 40 websites each",
-          "Utilized SEO tools (WordPress, SEOquake, Google Analytics, Search Console) to optimize performance",
+          "Contributed to ranking 40+ websites/articles on Google's first page for targeted keywords with SEO team.",
+          "Published 174 SEO-optimized articles.",
+          "Curated 150 high-authority social bookmarking websites (minimum DA 30, PA 30).",
+          "Social bookmarked 15 articles across a total of 40 websites each.",
+          "Utilized SEO tools (WordPress, SEOquake, Google Analytics, Search Console) to optimize performance.",
         ],
       },
     ],
@@ -234,29 +251,23 @@ export const cvSections: { [key: string]: CVSection } = {
       {
         title: "FILE/Filemagz Manager",
         organization: "Bina Nusantara Computer Club (BNCC)",
-        period: "Jan/2024 – Present",
+        period: "Jan/2024 – Jan/2025",
         details: [
-          "Lead a team of 6 people and conducted monthly sprints for better communication and ideation",
-          "Increased followers by over 2,600 in one year for the Filetechno Instagram account",
-          "Set a record for the most engaging reel with over 18,000 likes and 280,000 views",
-          "Achieved over 40,000+ views on 9 reels in a span of one year",
-          "Set a record for a microblog with 3,900 likes and 38,000 impressions",
-          "Achieved over 300+ likes on 37 microblog feeds in one year",
-          "Created a TikTok video that reached 13,000 likes and 170,000 views",
-          "Trained 32 activists in content creation, video editing, content writing, and copywriting",
-          "Mentored 13 activists in video editing using Adobe Premiere Pro for major annual video editing projects",
-          "Provided coverage for events such as Indo-Indians Bazaar, Creative Icon Exhibition, Event Collector Moments, BNCC x CTI Group, and BNCC x Apple Developer Academy",
-          "Established partnerships with Coding Studio, Indoindians, DKV Creative Advertising Binus, BNCC Kemanggisan, BNCC Region Alam Sutera, CakeResume, Dunia Coding, ToffeDev: SEOcon 2024, and Technoscape 2024, DevOpsDays Jakarta 2024",
+          "Led a team of 6 members, growing @filemagz & @filetechno followers by +2,600 in one year.",
+          "Mentored 32 activists in content creation & video editing, improving content quality.",
+          "Set a record for the most engaging reel with over 18,000 likes and 280,000 views.",
+          "Created a viral TikTok video with 170,000 views and 13,000 likes.",
+          "Achieved over 40,000+ views on 9 reels in a span of one year.",
+          "Achieved 300+ likes per post on 37 microblog feeds in one year.",
+          "Established partnerships with Apple Developer Academy, CTI Group, CakeResume, ToffeDev, Coding Studio, Indoindians, DKV Creative Advertising BINUS, Dunia Coding, Technoscape 2024, and DevOpsDays Jakarta 2024.",
         ],
       },
       {
         title: "Publication Coordinator",
         organization: "TechnoScape 2024",
-        period: "Feb/2024 – Present",
+        period: "Feb/2024 – Dec/2024",
         details: [
-          "Lead a team of 8 individuals and conducted weekly sprints to enhance communication and ideation",
-          "Determined detailed settings for Instagram ads to achieve maximum results",
-          "Successfully managed and completed 45 publications on the @technoscapebncc Instagram account",
+          "Led a team of 8, managing 45+ publications for @technoscapebncc Instagram account.",
         ],
       },
       {
@@ -264,11 +275,11 @@ export const cvSections: { [key: string]: CVSection } = {
         organization: "Bina Nusantara Computer Club (BNCC)",
         period: "Nov/2022 – Dec/2023",
         details: [
-          "PRAP Project Manager (Nov 2022 - Dec 2022): Led a team of 9 in organizing a large-scale event prototype.",
-          "Leader Team for Leadership Development Program (Mar 2023 - Apr 2023): Managed an 8-member team, ensuring task completion.",
-          "Leader Team for FILE Activist Subdivision (Jun 2023 - Aug 2022): Led a 4-member team for weekly content production.",
-          "Publication Vice Coordinator at BNCC CSR 2023 (Jul 2023 - Oct 2023): Supported the main coordinator; edited promotional reels.",
-          "Sponsorship committee for BNCC Opening Season (Jul 2023 - Oct 2023): Listed and blasted sponsors, managed booth for 3 days.",
+          "PRAP Project Manager: Led a team of 9 in organizing a large-scale event prototype.",
+          "Leadership Development Program Leader: Managed an 8-member team, ensuring task completion.",
+          "FILE Activist Leader: Led a 4-member team for weekly content production.",
+          "Sponsorship Committee (BNCC Opening Season): Managed sponsors and documentation.",
+          "Publication Vice Coordinator (BNCC CSR 2023): Edited promotional reels.",
         ],
       },
       {
@@ -276,11 +287,9 @@ export const cvSections: { [key: string]: CVSection } = {
         organization: "MarshaOshi",
         period: "Dec/2020 – Jan/2023",
         details: [
-          "Led the social media team of 15 admins across platforms including Twitter/X, Instagram, TikTok, and Showroom Live",
-          "Increased the Twitter/X account followers from 700 to 7,000",
-          "Grew the Instagram account followers from 0 to 1,800",
-          "Grew the TikTok account followers from 0 to 1,600",
-          "Achieved an average of 527.000 tweet impressions per month during the last 5 months of my tenure",
+          "Led the social media team across platforms including Twitter/X, Instagram, TikTok, and Showroom Live.",
+          "Increased Twitter/X followers from 700 to 7K, Instagram from 0 to 1.8K, and TikTok from 0 to 1.6K.",
+          "Achieved an average of 527.000 tweet impressions per month during the last 5 months of my tenure.",
         ],
       },
     ],
